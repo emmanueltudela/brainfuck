a54c9c5e37b56679486953446ee27b8a33e9a78f
etudelagirau <emmanueltudela.pro@gmail.com>
2025-01-10 14:20:27 +0100

    Added struct definition for linked_list.h

--- include/linked_list.h
+++ include/linked_list.h
@@ -3,10 +3,20 @@
 
 #include <stdbool.h>
 
+// Made to work with char for brainfuck
 typedef struct llist sllist;
 
-// Value is NULL
+// Empty sllist is NULL
 sllist *sllist_create();
 void sllist_free(sllist *llist);
+
+bool sllist_empty(sllist *llist);
+
+sllist *sllist_append(sllist *llist, char val);
+sllist *sllist_prepend(sllist *llist, char val);
+
+char sllist_value(sllist *llist);
+
+sllist *sllist_remove(sllist *llist);
 
 #endif
