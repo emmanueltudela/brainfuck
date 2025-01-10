723400fde60f9fcdf7680785ffa7614d1c6ab0d6
etudelagirau <emmanueltudela.pro@gmail.com>
2025-01-10 13:53:01 +0100

    Added stack implementation and stack_empty declaration

--- include/stack.h
+++ include/stack.h
@@ -1,6 +1,8 @@
 #ifndef __STACK_H__
 #define __STACK_H__
 
+#include <stdbool.h>
+
 #define STACK_DEFL_SIZE 10
 
 // made to work with char for brainfuck
@@ -8,6 +10,8 @@ typedef struct stack sstack;
 
 sstack *stack_create();
 void stack_free(sstack *stack);
+
+bool stack_empty(sstack *stack);
 
 void stack_push(sstack *stack, char val);
 char stack_pop(sstack *stack);
---
+++ src/stack.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "stack.h"
+
+typedef struct stack {
+    char *arr;
+    int size;
+    int top;
+} sstack;
+
+sstack *stack_create() {
+    sstack *stack = malloc(sizeof(sstack));
+    if (!stack) return NULL;
+
+    char *arr = malloc(sizeof(char) * STACK_DEFL_SIZE);
+    if (!arr) {
+        free(stack);
+        return NULL;
+    }
+
+    stack->arr = arr;
+    stack->size = STACK_DEFL_SIZE;
+    stack->top = 0;
+}
+
+void stack_free(sstack *stack) {
+    free(stack->arr);
+    free(stack);
+}
+
+bool stack_empty(sstack *stack) {
+    if (!stack) {
+        fprintf(stderr, "Cannot get empty from NULL stack\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return stack->top == 0;
+}
+
+void stack_push(sstack *stack, char val);
+
+char stack_pop(sstack *stack) {
+    if (!stack) {
+        fprintf(stderr, "Cannot pop from NULL stack\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (stack_empty(stack)) {
+        fprintf(stderr, "Cannot pop from empty stack\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return stack->arr[stack->top - 1];
+}
