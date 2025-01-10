6010dcbb01a9b151572668b80a20fd198891f29e
etudelagirau <emmanueltudela.pro@gmail.com>
2025-01-10 12:52:36 +0100

    Added base stack_test code

--- tests/stack_test.c
+++ tests/stack_test.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <string.h>
+#include <assert.h>
+
+void test_stack_create() {
+}
+
+void test_stack_free() {
+}
+
+void test_stack_push() {
+}
+
+void test_stack_pop() {
+}
+
+void usage(char *exec) {
+    printf("%s test_name\n");
+}
+
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        usage();
+        return EXIT_FAILURE;
+    }
+
+    char *testname = argv[1];
+
+    if (strcmp(testname, "stack_create") == 0) {
+        test_stack_create();
+    } else if (strcmp(testname, "stack_free") == 0) {
+        test_stack_free();
+    } else if (strcmp(testname, "stack_push") == 0) {
+        test_stack_push();
+    } else if (strcmp(testname, "stack_pop") == 0) {
+        test_stack_pop();
+    }
+
     return EXIT_SUCCESS;
 }
